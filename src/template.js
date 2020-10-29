dcb25c39438b4484ae4bd246f17e30660b215b23
姚旭 <xu.yao@zeropartner.com>
2020-10-29 18:00:10 +0800

    add some

--- src/template.js
+++ src/template.js
@@ -1,1 +1,12 @@
-export default function template() {}
+import {createElement as h, render} from './render'
+
+export default function template() {
+  let node = h('div', {
+    id: "JS-Preview",
+    className: 'preview-mask',
+    style: "background: rgba(0,0,0,.65)"
+  });
+
+
+  return render(node)
+}
