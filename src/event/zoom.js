03239330571533504d70381bb28a0e43c1088cda
姚旭 <xu.yao@zeropartner.com>
2020-11-05 18:24:18 +0800

    鼠标滚轮默认事件阻止

--- src/event/zoom.js
+++ src/event/zoom.js
@@ -1,8 +1,8 @@
-import { on, isFirefox, isMobile, rafThrottle } from '../utils'
-import store from '../store'
-import setGesture from './pinch'
+import { on, isFirefox, isMobile, rafThrottle } from "../utils";
+import store from "../store";
+import setGesture from "./pinch";
 
-const mousewheelEventName = isFirefox() ? 'DOMMouseScroll' : 'mousewheel';
+const mousewheelEventName = isFirefox() ? "DOMMouseScroll" : "mousewheel";
 
 const Scale = (delta) => {
   const { $stage, previewOptions } = store.state;
@@ -11,38 +11,41 @@ const Scale = (delta) => {
   let scale = store.state.scale;
   scale = scale + diff;
   scale = scale < 0.3 ? 0.3 : scale > zoom ? zoom : scale;
-  store.commit('set_scale', scale)
-  $stage.style.transform = `translate(-50%, -50%) scale(${store.state.scale})`
-}
+  store.commit("set_scale", scale);
+  $stage.style.transform = `translate(-50%, -50%) scale(${store.state.scale})`;
+};
 
 // delta > 0 放大 反之 缩小
-const _mouseWheelHanlder = rafThrottle(e => {
+const _mouseWheelHanlder = rafThrottle((e) => {
   const delta = e?.wheelDelta ?? -e.detail;
-  e.preventDefault();
-  Scale(delta)
-})
+  Scale(delta);
 
+  if (e.preentDefault) {
+    ev.preventDefault();
+  }
+  return false;
+});
 
 const pinch = () => {
   const { $stage, previewOptions } = store.state;
   const pinchObj = setGesture($stage);
-  
-  pinchObj.gesturemove = function (e) {  //双指移动
+
+  pinchObj.gesturemove = function (e) {
+    //双指移动
     let zoom = previewOptions?.zoom ?? 5;
     let diff = e.scale > 1 ? e.scale * 0.035 : e.scale * -0.035;
     let scale = store.state.scale;
-    scale = scale + diff
+    scale = scale + diff;
     scale = scale < 0.3 ? 0.3 : scale > zoom ? zoom : scale;
-    store.commit('set_scale', scale)
-    $stage.style.transform = `translate(-50%, -50%) scale(${store.state.scale})`
+    store.commit("set_scale", scale);
+    $stage.style.transform = `translate(-50%, -50%) scale(${store.state.scale})`;
   };
-  
-}
+};
 
 export default function zoom() {
   if (isMobile()) {
-    pinch()
+    pinch();
   } else {
-    on(document, mousewheelEventName, _mouseWheelHanlder)
+    on(document, mousewheelEventName, _mouseWheelHanlder);
   }
 }
